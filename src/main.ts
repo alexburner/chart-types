2674bbfa85fe3d1c6409b673a8d625c95118e8af
Alex Burner <alex.burner@crowdstrike.com>
2023-01-23 12:55:53 -0800

    Remove Vite starter code

--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,6 @@
-import { setupCounter } from './counter'
 import './style.css'
-import typescriptLogo from './typescript.svg'
 
 const appEl = document.getElementById('app')
 if (!appEl) throw new Error('Missing #app')
 
-appEl.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
-
-const buttonEl = document.getElementById('counter')
-if (!buttonEl) throw new Error('Missing #counter')
-if (!(buttonEl instanceof HTMLButtonElement)) throw new Error('Bad Button')
-
-setupCounter(buttonEl)
+appEl.innerHTML = 'Hello World'
